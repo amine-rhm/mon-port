a5784215603511cd125aeb6506f2d452deca6c53
RAHMOUNI Mohamed Amine <144079280+amine-rhm@users.noreply.github.com>
2025-09-08 15:02:28 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';  // <- note bien le ./ ici
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // Commenté temporairement pour éviter les erreurs React 19
   // <React.StrictMode>
     <App />
   // </React.StrictMode>
